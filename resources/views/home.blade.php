ce4f4570bdc21ccde75f6e76322193a43fe80823
marmanvii <marmanvi@gmail.com>
2019-04-10 16:19:09 -0400

    Filled home and fixed materialize

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,1 +1,10 @@
-
+@extends('layout.master')
+@section('title')
+  <title>Página de Inicio</title>
+@endsection
+@section('styles')
+  @include('layout.materialize')
+@endsection
+@section('body')
+  {{-- Contenido --}}
+@endsection
