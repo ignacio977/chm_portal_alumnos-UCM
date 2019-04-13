5189eabe24123fd3f23460c0a1e0dd065a09dc2d
marmanvii <marmanvi@gmail.com>
2019-04-13 19:11:17 -0400

    Commented Sidenav

--- resources/views/layout/nav.blade.php
+++ resources/views/layout/nav.blade.php
@@ -1,28 +1,30 @@
+{{-- Navbar --}}
 <nav>
-    <div class="nav-wrapper">
-      <a href="#!" class="brand-logo">UCM</a>
-      <a href="#" data-target="slide-out" class="sidenav-trigger"><i class="material-icons">menu</i></a>
-      <ul class="right hide-on-med-and-down">
-        <li><a href="#user"><img class="circle" height="50" align="middle" style="transform: translateY(-15%);" src="images/smile.png"></a></li>
-        <li><a href="sass.html">Alumnos</a></li>
-        <li><a href="badges.html">Cursos</a></li>
-        <li><a href="collapsible.html">Información</a></li>
-      </ul>
-    </div>
-  </nav>
+  <div class="nav-wrapper">
+    <a href="#!" class="brand-logo">UCM</a>
+    <a href="#" data-target="slide-out" class="sidenav-trigger"><i class="material-icons">menu</i></a>
+    <ul class="right hide-on-med-and-down">
+      <li><a href="#user"><img class="circle" height="50" align="middle" style="transform: translateY(-15%);" src="images/smile.png"></a></li>
+      <li><a href="sass.html">Alumnos</a></li>
+      <li><a href="badges.html">Cursos</a></li>
+      <li><a href="collapsible.html">Información</a></li>
+    </ul>
+  </div>
+</nav>
 
-  <ul id="slide-out" class="sidenav">
-    <li><div class="user-view">
-      <div class="background">
-        <img src="images/forest.jpg">
-      </div>
-      <a href="#user"><img class="circle" src="images/smile.png"></a>
-      <a href="#name"><span class="white-text name">Mike John</span></a>
-      <a href="#email"><span class="white-text email">correolindo@gmail.com</span></a>
-    </div></li>
-    <li><a href="#!">Alumnos</a></li>
-    <li><a href="#!">Cursos</a></li>
-    <li><div class="divider"></div></li>
-    <li><a class="subheader">Extra</a></li>
-    <li><a class="waves-effect" href="#!">Información</a></li>
-  </ul>
+{{-- Sidenav --}}
+<ul id="slide-out" class="sidenav">
+  <li><div class="user-view">
+    <div class="background">
+      <img src="images/forest.jpg">
+    </div>
+    <a href="#user"><img class="circle" src="images/smile.png"></a>
+    <a href="#name"><span class="white-text name">Mike John</span></a>
+    <a href="#email"><span class="white-text email">correolindo@gmail.com</span></a>
+  </div></li>
+  <li><a href="#!">Alumnos</a></li>
+  <li><a href="#!">Cursos</a></li>
+  <li><div class="divider"></div></li>
+  <li><a class="subheader">Extra</a></li>
+  <li><a class="waves-effect" href="#!">Información</a></li>
+</ul>
