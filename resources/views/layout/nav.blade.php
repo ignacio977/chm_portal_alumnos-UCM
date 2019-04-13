1dda69b6bfb7ae228e6dcd18a04b94e850b5406c
marmanvii <marmanvi@gmail.com>
2019-04-13 19:09:46 -0400

    Added Side Nav

--- resources/views/layout/nav.blade.php
+++ resources/views/layout/nav.blade.php
@@ -1,19 +1,28 @@
 <nav>
     <div class="nav-wrapper">
-      <a href="#!" class="brand-logo">Logo</a>
-      <a href="#" data-target="mobile-demo" class="sidenav-trigger"><i class="material-icons">menu</i></a>
+      <a href="#!" class="brand-logo">UCM</a>
+      <a href="#" data-target="slide-out" class="sidenav-trigger"><i class="material-icons">menu</i></a>
       <ul class="right hide-on-med-and-down">
-        <li><a href="sass.html">Sass</a></li>
-        <li><a href="badges.html">Components</a></li>
-        <li><a href="collapsible.html">Javascript</a></li>
-        <li><a href="mobile.html">Mobile</a></li>
+        <li><a href="#user"><img class="circle" height="50" align="middle" style="transform: translateY(-15%);" src="images/smile.png"></a></li>
+        <li><a href="sass.html">Alumnos</a></li>
+        <li><a href="badges.html">Cursos</a></li>
+        <li><a href="collapsible.html">Información</a></li>
       </ul>
     </div>
   </nav>
 
-  <ul class="sidenav" id="mobile-demo">
-    <li><a href="sass.html">Sass</a></li>
-    <li><a href="badges.html">Components</a></li>
-    <li><a href="collapsible.html">Javascript</a></li>
-    <li><a href="mobile.html">Mobile</a></li>
+  <ul id="slide-out" class="sidenav">
+    <li><div class="user-view">
+      <div class="background">
+        <img src="images/forest.jpg">
+      </div>
+      <a href="#user"><img class="circle" src="images/smile.png"></a>
+      <a href="#name"><span class="white-text name">Mike John</span></a>
+      <a href="#email"><span class="white-text email">correolindo@gmail.com</span></a>
+    </div></li>
+    <li><a href="#!">Alumnos</a></li>
+    <li><a href="#!">Cursos</a></li>
+    <li><div class="divider"></div></li>
+    <li><a class="subheader">Extra</a></li>
+    <li><a class="waves-effect" href="#!">Información</a></li>
   </ul>
