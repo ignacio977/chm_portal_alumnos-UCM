f50f1d2c2aa13975c8bc2d762b395b74ede9e2dd
marmanvii <marmanvi@gmail.com>
2019-05-09 19:49:34 -0400

    Added companies table

---
+++ database/migrations/2019_05_09_234544_create_empresas_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateEmpresasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('empresas', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->foreign('id_referencia')->references('id')->on('users')->onDelete('cascade');
+            $table->string('nombre');
+            $table->string('direccion');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('empresas');
+    }
+}
