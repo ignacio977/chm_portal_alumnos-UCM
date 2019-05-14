2caad1653596631642b2c0a41252d2bba389ee6f
marmanvii <marmanvi@gmail.com>
2019-05-14 17:14:25 -0400

    Updated jquery

--- resources/views/layout/materialize.blade.php
+++ resources/views/layout/materialize.blade.php
@@ -4,7 +4,7 @@
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
 
 <!-- Compiled and minified JavaScript -->
-<script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
+<script src="https://code.jquery.com/jquery-3.4.1.min.js" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
 
 <!--Let browser know website is optimized for mobile-->
