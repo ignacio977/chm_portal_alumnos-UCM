bfa6ba83cf90973c071e6f7221e2fa5db8c2dfdf
Claudio Durán <claudiodurannunez13@gmail.com>
2019-05-26 19:55:39 -0400

    Added student Information

--- resources/views/Estudiantes/index.blade.php
+++ resources/views/Estudiantes/index.blade.php
@@ -12,24 +12,22 @@
 @endsection
 
 @section('body')
-@foreach ($students as $student) {{-- Obtención de los datos del estudiante --}}
-    @if ($student->id == Auth::user()->id) {{-- TODO: Arreglar estética --}}
-    
-      Nombre: {{$student->nombres}}
-      {{$student->apellidos}}
-      <br>
-      Email: {{$student->email}}
-      <br>
-      Dirección: {{$student->direccion_actual}}
-      <br>
-      Telefono: {{$student->telefono}}
-      <br>
-      Celular: {{$student->celular}}
-      <br>
-      Fecha de Ingreso: {{$student->fecha_ingreso}}
-    @endif
-@endforeach
-
+  @foreach ($students as $student) {{-- Obtención de los datos del estudiante --}}
+      @if ($student->id == Auth::user()->id) {{-- TODO: Arreglar estética --}} 
+        Nombre: {{$student->nombres}}
+        {{$student->apellidos}}
+        <br>
+        Email: {{$student->email}}
+        <br>
+        Dirección: {{$student->direccion_actual}}
+        <br>
+        Telefono: {{$student->telefono}}
+        <br>
+        Celular: {{$student->celular}}
+        <br>
+        Fecha de Ingreso: {{date("d-m-Y", strtotime($student->fecha_ingreso))}} {{-- cambiamos el formato para mostrar la fecha --}}    
+      @endif
+  @endforeach
 @endsection
 
 @section('scripts')
