927cd9d9ec135129ec751951c95a234163cabe66
BernyDesh <berny.aguayo.tejos@gmail.com>
2019-05-27 04:02:34 -0400

    Collapsible added

--- resources/views/Estudiantes/index.blade.php
+++ resources/views/Estudiantes/index.blade.php
@@ -25,67 +25,56 @@
               <img src="/images/smile.png") style="width:40%">
             </div>
 
-              <h4><i class="material-icons">person</i>&nbsp{{$student->nombres}}<br>&nbsp&nbsp&nbsp&nbsp{{$student->apellidos}}
+              <h4><i class="material-icons">person</i>&nbsp{{$student->nombres}}&nbsp{{$student->apellidos}}
               </h4> 
               
               <div class="divider"></div>
               <div class="section">
-                <h5>Fecha de Ingreso</h5>
-                <p>{{date("d-m-Y", strtotime($student->fecha_ingreso))}}</p>
+                <h5><b>RUT</b></h5>
+                <p>{{$student->rut}}</p>
               </div>
               <div class="divider"></div>
               <div class="section">
-                <h5>Section 2</h5>
-                <p>Stuff</p>
+                <h5><b>Fecha de Ingreso</b></h5>
+                <p>{{date("d-m-Y", strtotime($student->fecha_ingreso))}}</p>
               </div>
+
              
             </div>
           </div>
           
           <div class="col s8">
-            <div class="card-panel z-depth-5">
-              <h4><i class="material-icons">chrome_reader_mode</i>&nbspInformación Personal</h4> 
-
-              <div class="divider"></div>
-              <div class="section">
-                <h5>Email</h5>
-                <p>{{$student->email}}</p>
-              </div>
-              <div class="divider"></div>
-              <div class="section">
-                <h5>Dirección</h5>
-                <p>{{$student->direccion_actual}}</p>
-              </div>
-              <div class="divider"></div>
-              <div class="section">
-                <h5>Teléfono</h5>
-                <p>{{$student->telefono}}</p>
-              </div>
-              <div class="divider"></div>
-              <div class="section">
-                <h5>Celular</h5>
-                <p>{{$student->celular}}</p>
-              </div>
-
-              
+            <div class="card-panel z-depth-4">
+              <h4>Bienvenido, Estudiante&nbsp<i class="material-icons">insert_emoticon</i></h4> 
             </div>
 
             <ul class="collapsible">
               <li>
-                <div class="collapsible-header"><i class="material-icons">filter_drama</i>First</div>
+                <div class="collapsible-header"><i class="material-icons">chrome_reader_mode</i>
+                  &nbsp<b>Información Personal</b></h6> </div>
                 <div class="collapsible-body">
                   <span>
-
+                    <div class="section">
+                      <h4>Email</h4>
+                      <p>{{$student->email}}</p>
+                    </div>
+                    <div class="divider"></div>
+                    <div class="section">
+                      <h4>Dirección</h4>
+                      <p>{{$student->direccion_actual}}</p>
+                    </div>
+                    <div class="divider"></div>
+                    <div class="section">
+                      <h4>Teléfono</h4>
+                      <p>{{$student->telefono}}</p>
+                    </div>
+                    <div class="divider"></div>
+                    <div class="section">
+                      <h4>Celular</h4>
+                      <p>{{$student->celular}}</p>
+                    </div> 
                   </span>
                 </div>
-              </li>
-              <li>
-                <div class="collapsible-header"><i class="material-icons">place</i>Second</div>
-                <div class="collapsible-body"><span>Lorem ipsum dolor sit amet.</span></div>
-              </li>
-              <li>
-                <div class="collapsible-header"><i class="material-icons">whatshot</i>Third</div>
-                <div class="collapsible-body"><span>Lorem ipsum dolor sit amet.</span></div>
               </li>
             </ul>
             
