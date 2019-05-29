d71726bf14b9595d07ffc38ef2faaa08d713e6ff
Gonzalo Guzmán Caamaño <gonza9611@hotmail.com>
2019-05-29 02:46:14 -0400

    Career-director-design

--- resources/views/Director/index.blade.php
+++ resources/views/Director/index.blade.php
@@ -14,16 +14,82 @@
 @section('body')
   @foreach ($directors as $director) {{-- Obtención de los datos del Director de Carrera --}}
     @if ($director->id == Auth::user()->id) {{-- TODO: Arreglar estética --}} 
-      Nombre: {{$director->nombres}}
-      {{$director->apellidos}}
-      <br>
-      Email: {{$director->email}}
-      <br>
-      Dirección: {{$director->direccion_actual}}
-      <br>
-      Telefono: {{$director->telefono}}
-      <br>
-      Celular: {{$director->celular}} 
+      <div class="row">
+        <div class="col s4">     
+            <div class="card-panel z-depth-5"> <!--Rectangulito donde estará el título y el botón desplegable -->
+              <?php $direccion_imagen = Auth::user()->foto ?>
+              <div align="center">                 
+                <img src="{{ URL::asset("{$direccion_imagen}") }}" style="width:40%">
+              </div>
+              <h4><i class="material-icons">person</i>&nbsp{{$director->nombres}}&nbsp{{$director->apellidos}}</h4> 
+              
+              <div class="divider"></div>
+              <div class="section">
+                <h5>RUT</h5> 
+                <p>{{$director->rut}}</p>
+              </div>
+            </div>
+        </div>      
+        <div class="col s8">            
+            <div class="card-panel z-depth-5"> <!--Rectangulito donde estará el título y el botón desplegable -->
+              <h4>Bienvenido Director de Carrera</h4>  
+            </div>
+            <ul class="collapsible"> <!--Collapsible de información-->
+            <li>
+              <div class="collapsible-header"><i class="material-icons">chrome_reader_mode</i>
+                &nbsp<b>Información Personal</b></h6> </div>
+              <div class="collapsible-body">
+                <span>
+                  <div class="section">
+                    <h7><b>Email</b></h7>
+                    <p><i>&nbsp&nbsp{{$director->email}}</i></p>
+                  </div> 
+                </span>
+              </div>
+              <div class="collapsible-body">
+                <span>
+                    <div class="section">
+                      <h7><b>Dirección</b></h7>
+                      <p><i>&nbsp&nbsp{{$director->direccion_actual}}</i></p>
+                    </div> 
+                  </span>
+              </div>
+              <div class="collapsible-body">
+                <span>
+                  <div class="section">
+                    <h7><b>Teléfono</b></h7>
+                    <p><i>&nbsp&nbsp{{$director->telefono}}</i></p>
+                  </div> 
+                </span>
+              </div>
+              <div class="collapsible-body">
+                <span>
+                  <div class="section">
+                    <h7><b>Celular</b></h7>
+                    <p><i>&nbsp&nbsp{{$director->celular}}</i></p>
+                  </div> 
+                </span>
+              </div>
+            </li>
+          </ul>
+
+          <ul class="collapsible"> <!--Collapsible de información-->
+            <li>
+              <div class="collapsible-header"><i class="material-icons">chrome_reader_mode</i>
+                &nbsp<b>Información 2</b></h6> </div>
+              <div class="collapsible-body">
+                <span>
+                  <div class="section">
+                    <h7><b>Ejemplo título</b></h7>
+                    <p><i>&nbsp&nbspInformación respectiva al ejemplo.</i></p>
+                  </div> 
+                </span>
+              </div>
+            </li>
+          </ul>
+        </div>      
+      </div>
+
     @endif
   @endforeach
 @endsection
