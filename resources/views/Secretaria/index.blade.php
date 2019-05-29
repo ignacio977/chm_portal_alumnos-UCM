d372a0a54007971c230ad8ec2e3858fe862061b3
Gonzalo Guzmán Caamaño <gonza9611@hotmail.com>
2019-05-29 02:36:46 -0400

    Secretary view design

--- resources/views/Secretaria/index.blade.php
+++ resources/views/Secretaria/index.blade.php
@@ -14,17 +14,82 @@
 @section('body')
   @foreach ($secretaries as $secretary) {{-- Obtención de los datos de la secretaria --}}
     @if ($secretary->id == Auth::user()->id) {{-- TODO: Arreglar estética --}} 
-      Nombre: {{$secretary->nombres}} {{$secretary->apellidos}}
-      <br>
-      Rut: {{$secretary->rut}}
-      <br>
-      Email: {{$secretary->email}}
-      <br>
-      Dirección: {{$secretary->direccion_actual}}
-      <br>
-      Telefono: {{$secretary->telefono}}
-      <br>
-      Celular: {{$secretary->celular}}    
+      <div class="row">
+        <div class="col s4">     
+           <div class="card-panel z-depth-5"> <!--Rectangulito donde estará el título y el botón desplegable -->
+              <?php $direccion_imagen = Auth::user()->foto ?>
+              <div align="center">                 
+                <img src="{{ URL::asset("{$direccion_imagen}") }}" style="width:40%">
+              </div>
+              <h4><i class="material-icons">person</i>&nbsp{{$secretary->nombres}}&nbsp{{$secretary->apellidos}}</h4> 
+              
+              <div class="divider"></div>
+              <div class="section">
+                <h5>RUT</h5> 
+                <p>{{$secretary->rut}}</p>
+              </div>
+           </div>
+        </div>      
+        <div class="col s8">            
+           <div class="card-panel z-depth-5"> <!--Rectangulito donde estará el título y el botón desplegable -->
+              <h4>Bienvenido/a secretario/a</h4>  
+           </div>
+           <ul class="collapsible"> <!--Collapsible de información-->
+            <li>
+              <div class="collapsible-header"><i class="material-icons">chrome_reader_mode</i>
+                &nbsp<b>Información Personal</b></h6> </div>
+              <div class="collapsible-body">
+                <span>
+                  <div class="section">
+                    <h7><b>Email</b></h7>
+                    <p><i>&nbsp&nbsp{{$secretary->email}}</i></p>
+                  </div> 
+                </span>
+              </div>
+              <div class="collapsible-body">
+                <span>
+                    <div class="section">
+                      <h7><b>Dirección</b></h7>
+                      <p><i>&nbsp&nbsp{{$secretary->direccion_actual}}</i></p>
+                    </div> 
+                  </span>
+              </div>
+              <div class="collapsible-body">
+                <span>
+                  <div class="section">
+                    <h7><b>Teléfono</b></h7>
+                    <p><i>&nbsp&nbsp{{$secretary->telefono}}</i></p>
+                  </div> 
+                </span>
+              </div>
+              <div class="collapsible-body">
+                <span>
+                  <div class="section">
+                    <h7><b>Celular</b></h7>
+                    <p><i>&nbsp&nbsp{{$secretary->celular}}</i></p>
+                  </div> 
+                </span>
+              </div>
+            </li>
+          </ul>
+
+          <ul class="collapsible"> <!--Collapsible de información-->
+            <li>
+              <div class="collapsible-header"><i class="material-icons">chrome_reader_mode</i>
+                &nbsp<b>Información 2</b></h6> </div>
+              <div class="collapsible-body">
+                <span>
+                  <div class="section">
+                    <h7><b>Ejemplo título</b></h7>
+                    <p><i>&nbsp&nbspInformación respectiva al ejemplo.</i></p>
+                  </div> 
+                </span>
+              </div>
+            </li>
+          </ul>
+        </div>      
+      </div>
+
     @endif
   @endforeach
 @endsection
