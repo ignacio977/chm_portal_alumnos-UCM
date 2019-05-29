c3f4635322bfcf8bbb0173a4bfd2b22948e8b369
Gonzalo Guzmán Caamaño <gonza9611@hotmail.com>
2019-05-29 17:21:50 -0400

    Minor view format changes

--- resources/views/Director/index.blade.php
+++ resources/views/Director/index.blade.php
@@ -29,8 +29,8 @@
               
               <div class="divider"></div>
               <div class="section">
-                <h5>RUT</h5> 
-                <p>{{$director->rut}}</p>
+                <h5><b>RUT</b></h5>
+                <p><b><i>&nbsp&nbsp{{$director->rut}}</i></b></p>
               </div>
             </div>
         </div>      
