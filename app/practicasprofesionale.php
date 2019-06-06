3562c623857bab90eaa650f86d8e82768178acd6
Savio Campos <sak_psx@hotmail.com>
2019-06-05 21:33:45 -0400

    Cambio de estado para practicas aceptadas y postulaciones desechadas

--- app/practicasprofesionale.php
+++ app/practicasprofesionale.php
@@ -4,7 +4,7 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class practicasprofesionale extends Model
+class Practicasprofesionale extends Model
 {
     protected $fillable = [
         'id', 'EmpresaId', 'DiasDesde',
