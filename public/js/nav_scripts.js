4ccf3a02e05aa5d0cfbc1703c8d284aafb2279df
Claudio Durán <claudiodurannunez13@gmail.com>
2019-06-06 20:41:15 -0400

    Fixed nav_scripts.js

--- public/js/nav_scripts.js
+++ public/js/nav_scripts.js
@@ -7,6 +7,5 @@ $(document).ready(function(){
       selectMonths: true, // Creates a dropdown to control month
       selectYears: 15, // Creates a dropdown of 15 years to control year
       format: 'yyyy-mm-dd' });
-     });
     $('.collapsible').collapsible(); 
 });
