184d2c6978ccd0c1afe697fb6bd435124218534e
coderiksando <eriksandovalsepulveda@gmail.com>
2019-06-25 05:55:55 -0400

    Cambios en los sentidos de notificacion

--- resources/views/layout/nav.blade.php
+++ resources/views/layout/nav.blade.php
@@ -15,16 +15,36 @@
     <a href="/home" class="brand-logo" align="middle">UCM</a>
     <ul class="right hide-on-med-and-down">
       @if(Auth::user())
-        <li><a class="dropdown-trigger tooltipped waves-effect waves-light" data-target='DropdownNotificacion' data-position="left" data-tooltip="Notificaciones"><i class="material-icons">public</i></a></li>
-        <ul id='DropdownNotificacion' class='dropdown-content'>
-          <li><a class="blue-text text-darken-2" href="/estudiante/novedadespractica"><i class="material-icons">work</i>Novedades de Practica</a></li>
-          <li class="divider" tabindex="-1"></li>
-          <li><a class="blue-text text-darken-2" href="#!">two</a></li>
-          <li class="divider" tabindex="-1"></li>
-          <li><a class="blue-text text-darken-2" href="#!">three</a></li>
-          {{-- <li><a href="#!"><i class="material-icons">view_module</i>four</a></li>
-          <li><a href="#!"><i class="material-icons">cloud</i>five</a></li> --}}
-        </ul>
+        <!-- {{$ArregloVisto=Auth::user()->PostulacionPractica->pluck('inspeccionado')}} -->
+        <!-- {{$ArregloUpdate=Auth::user()->PostulacionPractica->pluck('updated_at')}} -->
+        <!-- {{$contador=0}} -->
+        <!-- {{$notificacion=0}} -->
+        @foreach ($ArregloVisto as $FechaVisto)
+          @if($FechaVisto < $ArregloUpdate[$contador])
+            <!-- {{$notificacion=$notificacion+1}} -->
+            Registro
+          @endif  
+          <!-- {{$contador=$contador+1}} -->
+        @endforeach
+        @if($notificacion>0)
+          <li><a class="red dropdown-trigger tooltipped waves-effect waves-light" data-target='DropdownNotificacion' data-position="left" data-tooltip="Notificaciones"><i class="material-icons">public</i></a></li>
+          <ul id='DropdownNotificacion' class='dropdown-content'>
+            <li><a class="red blue-text text-darken-2" href="/estudiante/novedadespractica"><i class="material-icons">work</i>Novedades de Practica</a></li>
+            <li class="divider" tabindex="-1"></li>
+            <li><a class="blue-text text-darken-2" href="#!">two</a></li>
+            <li class="divider" tabindex="-1"></li>
+            <li><a class="blue-text text-darken-2" href="#!">three</a></li>
+          </ul>
+        @else
+          <li><a class="dropdown-trigger tooltipped waves-effect waves-light" data-target='DropdownNotificacion' data-position="left" data-tooltip="Notificaciones"><i class="material-icons">public</i></a></li>
+          <ul id='DropdownNotificacion' class='dropdown-content'>
+            <li><a class="blue-text text-darken-2" href="/estudiante/novedadespractica"><i class="material-icons">work</i>Novedades de Practica</a></li>
+            <li class="divider" tabindex="-1"></li>
+            <li><a class="blue-text text-darken-2" href="#!">two</a></li>
+            <li class="divider" tabindex="-1"></li>
+            <li><a class="blue-text text-darken-2" href="#!">three</a></li>
+          </ul>
+        @endif
       @endif
       <li><a href="">Botón 1</a></li>
       <li><a href="">Botón 2</a></li>
