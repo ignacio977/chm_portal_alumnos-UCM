ca155134a9380b4ff9ed37b91d6a99638a668206
elxexogit config --global user.name elxexogit config --global user.name elxexo <elxexo69@gmail.com>
2019-07-04 00:10:19 -0400

    cambios

--- routes/web.php
+++ routes/web.php
@@ -28,7 +28,8 @@ Route::get('/estudiante/practicasofertadas', 'EstudiantesController@catalogoprac
 Route::get('/estudiante/practicasofertadas/detalle', 'EstudiantesController@practicasdetalle')->name('DetallePractica');
 Route::get('/estudiante/evaluacionpractica', 'EstudiantesController@evaluacionpractica')->name('EvaluarPractica');
 Route::post('/estudiante/evaluacionpractica', 'EstudiantesController@evaluacionpracticaenvio')->name('EvaluarPracticaEnvio');
-
+Route::get('/estudiante/novedadespractica', 'EstudiantesController@novedadespractica')->name('NovedadesPractica');
+Route::get('/estudiante/vistopractica', 'EstudiantesController@VistoPractica')->name('VistoPractica');
 
 #Profesores#
 Route::get('/profesor', 'ProfesoresController@index')->name('profesor');
@@ -39,6 +40,7 @@ Route::get('/profesor/coordinador', 'CoordinadorController@AprobarPracticas')->n
 Route::post('/profesor/coordinador', 'CoordinadorController@CambiarEstado')->name('CambiarEstado');
 Route::get('/profesor/coordinador/practicasconcluidas', 'CoordinadorController@PracticasConcluidas')->name('PracticasConcluidas');
 Route::get('/profesor/coordinador/practicasconcluidasdetalle', 'CoordinadorController@PracticasConcluidasDetalle')->name('PracticasConcluidasDetalle');
+Route::get('/profesor/coordinador/PracticaActual','CoordinadorController@PracticaEnCurso')->name('PracticasActuales');
 #Director#
 Route::get('/director', 'DirectorController@index')->name('director');
 
@@ -47,7 +49,6 @@ Route::get('/secretaria', 'SecretariaController@index')->name('secretaria');
 
 #Empresa#
 Route::get('/empresa', 'EmpresaController@index')->name('empresa');
-
 Route::get('/empresa/practicas', 'EmpresaController@CreacionPracticasProfesionales');
 Route::post('/empresa/practicas/carga', 'EmpresaController@VerificacionPracticaProfesional');
 Route::post('/empresa/practicas/enviar', 'EmpresaController@InsercionPracticaProfesional');
@@ -58,3 +59,20 @@ Route::post('/empresa/practicas/editar', 'EmpresaController@VerificarPracticas')
 Route::get('/empresa/practicas/mostrar_finalizadas', 'EmpresaController@MostrarPracticasFinalizadas')->name('practicasfinalizadasempresa');
 Route::get('/empresa/practicas/finalizadas_evaluacion', 'EmpresaController@PracticasEvaluacion')->name('practicasevaluacion');
 Route::post('/empresa/practicas/evaluacionempresa', 'EmpresaController@Evaluacion')->name('evaluacionempresa');
+
+
+
+#Coordinador de practicas#
+Route::get('/profesor/coordinador', 'CoordinadorController@AprobarPracticas')->name('AprobarPracticas');
+Route::post('/profesor/coordinador', 'CoordinadorController@CambiarEstado')->name('CambiarEstado');
+Route::get('/profesor/coordinador/addE', 'CoordinadorController@AddEmpresa')->name('AddEmpresa');
+Route::post('/profesor/coordinador/addE', 'CoordinadorController@NuevaEmpresa')->name('NuevaEmpresa');
+Route::get('/profesor/practicas', 'CoordinadorController@VerPracticas')->name('VerPracticas');
+Route::get('/profesor/practicas/detalle', 'CoordinadorController@DetallePracticas')->name('DetalleCoordinacionPractica');
+Route::post('/profesor/EliminarPractica', 'CoordinadorController@EliminarPractica')->name('EliminarPractica');
+
+
+
+
+
+
